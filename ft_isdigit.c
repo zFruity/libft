61810f4251f72459ad2c4e7fbdc2f0b632903b54
zFruity <xlrshadow1@gmail.com>
2023-04-05 11:47:56 +0800

    added header to isdigit

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,11 +6,13 @@
 /*   By: dchuah <dchuah@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 17:52:58 by dchuah            #+#    #+#             */
-/*   Updated: 2023/03/29 18:09:55 by dchuah           ###   ########.fr       */
+/*   Updated: 2023/04/05 11:46:32 by dchuah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isdigit(int a)
+#include "libft.h"
+
+int ft_isdigit(int a)
 {
 	if (a >= '0' && a <= '9')
 		return (1);
