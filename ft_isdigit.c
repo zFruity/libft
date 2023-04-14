b95b3c60faa76742b73885da1e2cc82872ac72f6
zfruity <xlrshadow1@gmail.com>
2023-04-14 15:56:55 +0800

    asdf

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,13 +6,13 @@
 /*   By: dchuah <dchuah@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 17:52:58 by dchuah            #+#    #+#             */
-/*   Updated: 2023/04/05 11:46:32 by dchuah           ###   ########.fr       */
+/*   Updated: 2023/04/14 15:53:09 by dchuah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_isdigit(int a)
+int	ft_isdigit(int a)
 {
 	if (a >= '0' && a <= '9')
 		return (1);
